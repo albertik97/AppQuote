63664b3cd4dd39e1e16b1169c23c7e80f9a46341
Unknown <alberto.berenguer.97@gmail.com>
2018-06-27 19:20:28 +0200

    Pantalla principal 0.5v

--- AppQuote/www/scripts/index.js
+++ AppQuote/www/scripts/index.js
@@ -1,30 +1,10 @@
-﻿// Si quiere una introducción sobre la plantilla En blanco, vea la siguiente documentación:
-// http://go.microsoft.com/fwlink/?LinkID=397704
-// Para depurar código al cargar la página en cordova-simulate o en dispositivos o emuladores Android: inicie la aplicación, establezca puntos de interrupción 
-// y ejecute "window.location.reload()" en la Consola de JavaScript.
-(function () {
-    "use strict";
+﻿function prepararCanvas() {
+    let canvas = document.querySelectorAll("canvas");
+    let ctx = canvas[0].getContext('2d');
 
-    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+    ctx.arc(360, 70, 50, 0, (Math.PI / 180) * 360, true);
+    ctx.strokeStyle = "#f00";
+    ctx.lineWidth = 10;
+    ctx.stroke();
 
-    function onDeviceReady() {
-        // Controlar la pausa de Cordova y reanudar eventos
-        document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
-        
-        // TODO: Cordova se ha cargado. Haga aquí las inicializaciones que necesiten Cordova.
-        var parentElement = document.getElementById('deviceready');
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-    };
-
-    function onPause() {
-        // TODO: esta aplicación se ha suspendido. Guarde el estado de la aplicación aquí.
-    };
-
-    function onResume() {
-        // TODO: esta aplicación se ha reactivado. Restaure el estado de la aplicación aquí.
-    };
-} )();
+    }
