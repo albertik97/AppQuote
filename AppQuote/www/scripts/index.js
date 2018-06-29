1bb8308b363fa877b2d81936750525c2442baf8c
Unknown <alberto.berenguer.97@gmail.com>
2018-06-29 20:58:26 +0200

    Lee frases y el fichero bueno

--- AppQuote/www/scripts/index.js
+++ AppQuote/www/scripts/index.js
@@ -1,37 +1,15 @@
 ﻿var img;
-<<<<<<< HEAD
 var jsonAll;
-=======
 var data;
->>>>>>> Alberto
+
 function prepararCanvas() {
     let canvas = document.querySelector("#cv");
     let ctx = canvas.getContext('2d');
 
-    $.getJSON("frases.json", function (data) {
+    $.getJSON("data.json", function (data) {
         jsonAll = data;
         // ... Cargar el fichero json 1 sola vez
     });
-    /*
-    var img = new Image();
-    img.src = './images/cam.png';
-    img.onload = function () {
-        ctx.drawImage(img, canvas.width-40, 10, 30, 30);
-    };
-
-    ctx.beginPath();
-    ctx.arc(canvas.width / 2, canvas.height-60, 25, 0, (Math.PI / 180) * 360, true);
-    var grd = ctx.createRadialGradient(12,12, 10,12, 12, 15);
-    grd.addColorStop(0, "blue");
-    grd.addColorStop(1, "white")
-    ctx.strokeStyle = "blue";
-    ctx.lineWidth = 3;
-
-    ctx.fillStyle = grd;
-    ctx.stroke();
-    ctx.fill();*/
-
-
 }
 
 function alertDismissed() {
@@ -57,7 +35,7 @@ function cameraTakePicture() {
         img.src = imageData;
         img.onload = function () { 
             ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-            ctx.font = "bold 40px sans-serif";
+            ctx.font = "bold 20px sans-serif";
             var gradient= ctx.createLinearGradient(0, 0, canvas.width, 0);
             gradient.addColorStop("0", "red");
             gradient.addColorStop("0.5", "yellow");
@@ -65,13 +43,8 @@ function cameraTakePicture() {
             ctx.fillStyle = gradient;
             ctx.textAlign = "center";
             //let text = loadText();
-<<<<<<< HEAD
+
             ctx.fillText(loadText(), canvas.width / 2, canvas.height * 0.9);
-          
-=======
-            ctx.fillText("ARRIBA ESPAÑA", canvas.width / 2, canvas.height * 0.9);
-            data = ctx.getImageData(0, 0, canvas.width, canvas.height).toDataURL("image/png");
->>>>>>> Alberto
          };
     }
 
@@ -90,7 +63,7 @@ function loadText() {
 
     var indice = Math.floor(Math.random() * n);
 
-    return jsonAll['frases'][indice]['frase'];
+    return jsonAll['frases'][indice]['text'];
 
 }
 
